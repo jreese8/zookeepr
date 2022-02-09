c4a3d79298a40e0791f3d40531ae8e98174d3187
jreese8 <johnathanreese8@gmail.com>
2022-02-09 14:15:59 -0500

    Lesson 11-3

--- server.js
+++ server.js
@@ -9,14 +9,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
 
   function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -116,4 +114,24 @@ app.listen(PORT, () => {
       const animal = createNewAnimal(req.body, animals);
       res.json(animal);
     }
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
